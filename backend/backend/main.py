886c424520fdc71baeb9e57b33c19243f1404fc6
Ville <ville.reima@tietoa.fi>
2024-04-26 08:07:00 +0300

    added url_verification

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from typing import Literal
+
+from fastapi import FastAPI, Request
 from loguru import logger
+from pydantic import BaseModel
 
 from backend.config import settings
 
@@ -11,9 +14,21 @@ async def root():
     return {"details": "Root!"}
 
 
+class VerificationModel(BaseModel):
+    token: str
+    challenge: str
+    type: Literal["url_verification"]
+
+
 @app.post("/verification")
-async def url_verification(token: str, challenge: str, type: str):
+async def url_verification(body: VerificationModel):
     """
     Respond to the Slack verification request with plaintext.
     """
-    return challenge
+    return body.challenge
+
+
+@app.post("/debug")
+async def debug(request: Request):
+    logger.debug(request.body)
+    logger.debug(request)
