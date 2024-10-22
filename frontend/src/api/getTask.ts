31834167a79f242e218fdcc0a4a1cc37aee2e327
vireima <ville.reima@gmail.com>
2024-10-22 09:51:53 +0300

    frontend auth testing work

--- frontend/src/api/getTask.ts
+++ frontend/src/api/getTask.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { InputTask } from "../types/Task";
 import config from "../config";
+import useAuth from "../hooks/useAuth";
 
 export default async function getTask(channel: string, ts: string) {
   const response = await axios.get(
-    `https://${config.API_URL}/tasks/${channel}/${ts}`
+    `https://${config.API_URL}/tasks/${channel}/${ts}`,
+    { headers: { Authorization: `Bearer: ${useAuth()}` } }
   );
   return <InputTask>response.data[0];
 }
--- frontend/src/api/getTasks.ts
+++ frontend/src/api/getTasks.ts
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { InputTask } from "../types/Task";
 import config from "../config";
+import useAuth from "../hooks/useAuth";
 
 export default async function getTasks() {
   const response = await axios.get(
-    `https://${config.API_URL}/tasks?include_archived=true`
+    `https://${config.API_URL}/tasks?include_archived=true`,
+    { headers: { Authorization: `Bearer: ${useAuth()}` } }
   );
   return response.data as InputTask[];
 }
